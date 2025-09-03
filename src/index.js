3eeea988784de942ccd9516e64a18a9be65fc328
Katherine <katerina0708@gmail.com>
2025-09-03 15:22:49 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (matrix === undefined) {
+    return result;
+  }
+  for (let i = 0; i < matrix.length; i += 1) {
+    if (i % 2 === 0) {
+      result.push(...matrix[i]);
+    } else {
+      for (let j = 0; j < matrix[i].length; j += 1) {
+        result.push(matrix[i][matrix[i].length - 1 - j]);
+      }
+    }
+  }
+  return result;
 };
